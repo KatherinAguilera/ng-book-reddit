ce83dbe53fe9eccf10d8143083486f9cbe6d1760
unknown <katherine.aguilera@huelladigital.tv>
2019-12-16 16:46:04 -0500

    article

---
+++ angular8/angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.model'
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  article: Article;
+
+  constructor() {
+    this.article = new Article(
+       'Angular',
+       'http://angular.io',
+       10 );
+   }
+    voteUp(): boolean {
+     this.article.votes += 1;
+     return false;
+     }
+    
+     voteDown(): boolean {
+     this.article.votes -= 1;
+     return false;
+     }
+    
+  ngOnInit() {
+  }
+
+}
